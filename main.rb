6ced30c68ac1aa2cbc1ae1722154183b0c2fe8df
KennyUTN <asisignacio@gmail.com>
2020-12-11 15:04:03 -0300

    some methods

--- main.rb
+++ main.rb
@@ -22,3 +22,59 @@ module Enumerable
     my_each { |i| new_array << i if yield i }
     new_array
   end
+
+    def my_map
+      return self.dup unless block_given?
+
+      ary = []
+      self.each do |elem|
+        ary << yield(elem)
+      end
+
+      ary
+    end
+
+ary2 = [2, 3, 4]
+
+my_map
+
+
+
+
+
+def my_map
+    return self.dup unless block_given?
+
+    ary = []
+    self.my_each do |elem|
+      ary << yield(elem)
+    end
+
+    ary
+  end
+
+
+
+
+def my_select
+    return self.dup unless block_given?
+
+    ary = []
+    self.my_each do |elem|
+      if yield (elem)
+         ary << elem
+        end
+    end
+    ary
+  end
+  def my_reduce
+    return self.dup unless block_given?
+
+    ary = []
+    self.my_each do |elem|
+      if yield (elem)
+         ary << elem
+        end
+    end
+    ary
+  end
