d785538c245baf91a2c16dce84f830f30d51ce29
KennyUTN <asisignacio@gmail.com>
2020-12-15 17:24:40 -0300

    adding all the cases

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# rubocop: disable Metrics/ModuleLength, Style/ConditionalAssignment
+# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/MethodLength
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
@@ -26,12 +28,29 @@ module Enumerable
   end
 
   def my_all?
-    return nil unless block_given?
+    flag = 0
+    self.class == Hash || self.class == Range ? arr = to_a : arr = self
+    case args.empty?
+    when true
+      if block_given?
+        arr.my_each do |i|
+          break if yield(i).nil? || yield(i) == false
 
-    my_each do |elem|
-      return false unless yield elem
+          flag += 1 if yield(i) != false || yield(i).nil?
+        end
+      else
+        arr.my_each { |i| flag += 1 unless i.nil? || i == false }
+      end
+    when false
+      if args[0].class == Regexp
+        arr.my_each { |itr| flag += 1 if itr =~ args[0] }
+      elsif args[0].class == Class
+        arr.my_each { |itr| flag += 1 if itr.is_a?(args[0]) }
+      else
+        arr.my_each { |itr| flag += 1 if args[0] == itr }
+      end
     end
-    true
+    flag == size
   end
 
   def my_map(proc = nil)
@@ -47,35 +66,58 @@ module Enumerable
     ary
   end
 
-  def my_none?
-    return nil unless block_given?
-
-    my_each do |elem|
-      return false if yield elem
-    end
-    true
-  end
-
   def my_any?
-    return nil unless block_given?
-
-    my_each do |elem|
-      return true if yield elem
+    flag = 0
+    case args.empty?
+    when true
+      if block_given?
+        my_each do |i|
+          flag += 1 if block.call(i)
+        end
+      else
+        my_each do |i|
+          flag += 1 unless i.nil? || i == false
+        end
+      end
+    when false
+      if args[0].class == Regexp
+        my_each do |itr|
+          flag += 1 if itr =~ args[0]
+        end
+      elsif args[0].class == Class
+        my_each do |itr|
+          flag += 1 if itr.is_a?(args[0])
+        end
+      else
+        my_each do |itr|
+          flag += 1 if args[0] == itr
+        end
+      end
     end
-    false
+    flag.positive?
   end
 
-  def my_count
-    return size unless block_given?
+  def my_none?
+    !my_any?(*args, &block)
+  end
 
+  def my_count(*arg)
     n = 0
-    my_each do
-      n += 1
+    case !block_given?
+    when true
+      if arg.empty?
+        size
+      else
+        my_each { |i| n += 1 if arg[0] == i }
+        n
+      end
+    when false
+      my_each { |i| n += 1 if yield(i) == true }
       n
     end
   end
 
-  def my_inject(arg, sim)
+  def my_inject(arg = nil, sim = nil)
     if block_given?
       acc = arg
       my_each { |i| acc = acc.nil? ? i : yield(acc, i) }
@@ -93,6 +135,9 @@ module Enumerable
     end
   end
 end
+
+# rubocop: enable Metrics/ModuleLength, Style/ConditionalAssignment
+# rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/MethodLength
 def multiply_els(array)
   array.my_inject(:*)
 end
