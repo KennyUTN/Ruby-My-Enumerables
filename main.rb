1e94344455ca1375f45975c031eb4a0b5a2f8e22
KennyUTN <asisignacio@gmail.com>
2020-12-15 12:18:19 -0300

    yea

--- main.rb
+++ main.rb
@@ -24,18 +24,7 @@ module Enumerable
   end
 
   def my_map
-    return dup unless block_given?
-
-    ary = []
-    each do |elem|
-      ary << yield(elem)
-    end
-
-    ary
-  end
-
-  def my_map
-    return dup unless block_given?
+    return self.dup unless block_given?
 
     ary = []
     my_each do |elem|
@@ -43,7 +32,73 @@ module Enumerable
     end
 
     ary
+  end
+
+  def my_select
+    return self.dup unless block_given?
+
+    ary = []
+    my_each do |elem|
+      ary << elem if yield elem
     end
+    ary
+    end
+
+  def my_all?
+    return nil unless block_given?
+
+    my_each do |elem|
+      return false unless yield elem
+    end
+    true
+  end
+
+
+    def my_any?
+      return nil unless block_given?
+
+      my_each do |elem|
+        return true unless yield elem
+      end
+      false
+        end
+
+
+  def my_none?
+          return nil unless block_given?
+
+          my_each do |elem|
+            return false unless yield elem
+          end
+          true
+            end
+
+  def my_count
+              return self.size unless block_given?
+
+              n = 0
+              my_each do n += 1
+
+              n
+            end
+
+      end
+
+
+    end
+
+    testo = [1, 2, 3, 4, 3]
+
+puts testo.my_all? { |elem| elem < 8 }
+
+puts testo.my_any? { |elem| elem < 8 }
+
+puts testo.my_count
+
+
+
+    ary
+  end
 
   def my_select
     return dup unless block_given?
@@ -62,11 +117,74 @@ module Enumerable
       return false unless yield elem
     end
     true
-      end
+  end
+
+  def my_any?
+    return nil unless block_given?
+
+    my_each do |elem|
+      return true unless yield elem
     end
+    false
+      end
 
+  def my_none?
+    return nil unless block_given?
 
-testo=[1, 2, 3, 4, 5]
+    my_each do |elem|
+      return false unless yield elem
+    end
+    true
+      end
 
+  def my_count
+    return size unless block_given?
 
-print testo.my_all? {|elem| elem < 8 }
+    n = 0
+    my_each do
+      n += 1
+
+      n
+    end
+      end
+
+  def my_inject(var1 , var2)
+
+    return 'Error' if var1.nil? && var2.nil? && !block_given?
+
+    memo = nil
+    symbol = nil
+    unless var1.nil? && var2.nil?
+      memo = var1
+      symbol = var2
+      my_each do |i|
+        memo = memo.send(symbol, i)
+      end
+    if var1.is_a? Symbol
+      symbol = var1
+      my_each { |i| memo = (memo ? memo.send(symbol, i) : i) }
+    else
+      memo = var1
+      my_each { |i| memo = (memo ? yield(memo, i) : i) }
+    end
+    memo
+ end
+        end
+      end
+def multiply_els(arg = nil)
+
+        return 'Error' if arg.nil? || !arg.is_a?(Array)
+
+          arg.my_inject(:*)
+        end
+testo = [1, 2, 3]
+
+puts testo.my_all? { |elem| elem < 8 }
+
+puts testo.my_any? { |elem| elem < 8 }
+
+puts testo.my_count
+
+puts testo.my_inject(0 , :+)
+
+puts multiply_els(testo)
